aaf953adb575f9743a80a4a84c1d2ab61cec6ae0
Efren Aguilar <efrenaguilar@dhcp-v067-240.mobile.uci.edu>
2016-02-10 13:00:46 -0800

    What happens when you code in class. I got some basic analysis working, along with their accuracies. We may want to test them though, I got an 88% accuracy for Logistic Regression which seems to good to be true.

--- Yelp Dataset/yelp_review.py
+++ Yelp Dataset/yelp_review.py
@@ -2,6 +2,12 @@ import json, re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk import FreqDist
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 def tokenize_simple(text):
     #punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
@@ -44,15 +50,69 @@ def get_freq_dist(data):
         tokens += tokenize_simple(review['text'])
     tokens = remove_stopwords_inner(tokens, stopwords = stopwords.words('english'))
     return FreqDist(tokens)
+
+def get_count_vect(data):
+    tokens = []
+    count_vect = CountVectorizer(stop_words = stopwords.words("english"))
+    for review in data:
+        tokens += tokenize_simple(review['text'])
+    return count_vect.fit_transform(tokens)
     
 if __name__ == '__main__':
-    data = load_json_data('yelp_academic_dataset_review.json', 420000)
-    positive, negative = split_by_rating(data)
-    pos_dist = get_freq_dist(positive)
-    neg_dist = get_freq_dist(negative)
+    pass
+    """Need to try implementing text classifier as follows:
+        group text into 2 categories, positive and negative
+        create a transform/classifer to test and train on overall dataset
+        using these two categories
+        Run tests on this classifier
+        Find ways to improve it"""
     
+    data = load_json_data('yelp_academic_dataset_review.json', 10000)
+    train_target = []
+    positive_train, negative_train = split_by_rating(data[:9000])
+    train_target = np.array([1]*len(positive_train) + [0]*len(negative_train))
+    train_data = positive_train+negative_train
+    positive_test, negative_test = split_by_rating(data[9000:])
+    test_target = np.array([1]*len(positive_test) + [0]*len(negative_test))
+    test_data = positive_test + negative_test
+    count_vect = CountVectorizer(stop_words = stopwords.words("english"))
+    train_tokens = []
+    test_tokens = []
+    for review in train_data:
+        train_tokens += [review['text']]
+    for review in test_data:
+        test_tokens += [review['text']]
+    X_train_counts = count_vect.fit_transform(train_tokens)
+    X_test_counts = count_vect.transform(test_tokens)
+    tf_transformer = TfidfTransformer()
+    X_train_tfidf = tf_transformer.fit_transform(X_train_counts)
+    X_test_tfidf = tf_transformer.transform(X_test_counts)
+    clf = MultinomialNB()
+    clf.fit(X_train_tfidf, train_target)
+    predicted_multNB = clf.predict(X_test_tfidf)
+    clf = BernoulliNB()
+    clf.fit(X_train_tfidf, train_target)
+    predicted_bernoulliNB = clf.predict(X_test_tfidf)
     
-    print(pos_dist.most_common(69))
-    print ('\n\n')
-    print(neg_dist.most_common(69))
-            
+    clf = LogisticRegression().fit(X_train_tfidf, train_target)
+    predicted_LR = clf.predict(X_test_tfidf)    
+    print('Accuracy with multinomial naive Bayes: ', '%.4f'
+          % np.mean(predicted_multNB == test_target) )
+    print('Accuracy with Bernoulli naive Bayes: ', '%.4f'
+          % np.mean(predicted_bernoulliNB == test_target) )    
+    print('Accuracy with Logistic Regression: ', '%.4f'
+          % np.mean(predicted_LR == test_target) )
+#    pos_dist = get_freq_dist(positive)
+#    neg_dist = get_freq_dist(negative)
+#    pos_vect = get_count_vect(positive)
+#    #neg_vect = get_count_vect(negative)
+#    
+#    tf_transformer = TfidfTransformer();
+#    pos_transform = tf_transformer.fit_transform(pos_vect)
+#    #neg_transform = tf_transformer.transform(neg_vect)
+#    
+#    
+#    
+##    print(pos_dist.most_common(69))
+##    print ('\n\n')
+##    print(neg_dist.most_common(69))            
